07de63c79b79bcfc9a7481ac125eb0d9abac6f70
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-07-22 18:49:24 -0600

    Remove jacoco workaround

--- app/src/androidTest/java/com/sqisland/friendspell/util/MockTestRunner.java
+++ app/src/androidTest/java/com/sqisland/friendspell/util/MockTestRunner.java
@@ -5,38 +5,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnitRunner;
-import android.util.Log;
 
-import com.sqisland.friendspell.BuildConfig;
 import com.sqisland.friendspell.MockFriendSpellApplication;
 import com.sqisland.friendspell.dagger.MockDatabaseApiModule;
 
-import java.lang.reflect.Method;
-
-import timber.log.Timber;
-
-// http://stackoverflow.com/questions/30337375/empty-jacoco-report-for-android-espresso/31600193#31600193
-public class AndroidJacocoTestRunner extends AndroidJUnitRunner {
-  static {
-    System.setProperty("jacoco-agent.destfile",
-        "/data/data/" + BuildConfig.APPLICATION_ID + "/coverage.ec");
-  }
-
+public class MockTestRunner extends AndroidJUnitRunner {
   @Override
   public void finish(int resultCode, Bundle results) {
-    try {
-      Class rt = Class.forName("org.jacoco.agent.rt.RT");
-      Method getAgent = rt.getMethod("getAgent");
-      Method dump = getAgent.getReturnType().getMethod("dump", boolean.class);
-      Object agent = getAgent.invoke(null);
-      dump.invoke(agent, false);
-    } catch (Throwable e) {
-      Timber.e(Log.getStackTraceString(e));
-    }
-
     InstrumentationRegistry.getInstrumentation().getTargetContext().deleteDatabase
         (MockDatabaseApiModule.DATABASE_NAME);
-
     super.finish(resultCode, results);
   }
 
